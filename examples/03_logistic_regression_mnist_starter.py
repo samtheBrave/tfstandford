07435f525d434efc8ab0266b74dba90c12e4f905
samtheBrave <siaboonleong@hotmail.com>
2017-01-27 12:44:44 +0800

    new change

--- examples/03_logistic_regression_mnist_starter.py
+++ examples/03_logistic_regression_mnist_starter.py
@@ -11,9 +11,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 import time
 
 # Define paramaters for the model
-learning_rate = 0.01
+learning_rate = 0.003
 batch_size = 128
-n_epochs = 10
+n_epochs = 100
 
 # Step 1: Read in data
 # using TF Learn's built in function to load MNIST data to the folder data/mnist
@@ -23,60 +23,86 @@ mnist = input_data.read_data_sets('/data/mnist', one_hot=True)
 # each image in the MNIST data is of shape 28*28 = 784
 # therefore, each image is represented with a 1x784 tensor
 # there are 10 classes for each image, corresponding to digits 0 - 9. 
+X = tf.placeholder(tf.float32, [batch_size, 784], name='X_placeholder')
+Y = tf.placeholder(tf.float32, [batch_size, 10], name='Y_placeholder')
+
+
+L = 200
+M = 100
+N = 60
+O = 30
+
 
 
 # Step 3: create weights and bias
 # weights and biases are initialized to 0
 # shape of w depends on the dimension of X and Y so that Y = X * w + b
 # shape of b depends on Y
-
-
+W = tf.Variable(tf.random_normal(shape=[784, L], stddev=0.01), name='weights')
+B = tf.Variable(tf.zeros([1, L]), name="bias")
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+B2 = tf.Variable(tf.zeros([M]))
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+B3 = tf.Variable(tf.zeros([N]))
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(tf.zeros([O]))
+W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
 # Step 4: build model
 # the model that returns the logits.
 # this logits will be later passed through softmax layer
 # to get the probability distribution of possible label of the image
 # DO NOT DO SOFTMAX HERE
-
-
+Y1 = tf.nn.sigmoid(tf.matmul(X, W) + B)
+Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
+Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
+Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
+Ylogits = tf.matmul(Y4, W5) + B5
+logits = tf.nn.softmax(Ylogits)
 # Step 5: define loss function
 # use cross entropy loss of the real labels with the softmax of logits
 # use the method:
 # tf.nn.softmax_cross_entropy_with_logits(logits, Y)
 # then use tf.reduce_mean to get the mean loss of the batch
-
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y)
+cross_entropy = tf.reduce_mean(cross_entropy)*100
+#entropy = tf.nn.softmax_cross_entropy_with_logits(logits, Y, name='loss')
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(logits, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+loss = tf.reduce_mean(cross_entropy) # computes the mean over all the examples in the batch
 
 # Step 6: define training op
 # using gradient descent to minimize loss
-
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
 with tf.Session() as sess:
-	start_time = time.time()
-	sess.run(tf.global_variables_initializer())	
-	n_batches = int(mnist.train.num_examples/batch_size)
-	for i in range(n_epochs): # train the model n_epochs times
-		total_loss = 0
+    start_time = time.time()
+    sess.run(tf.global_variables_initializer())
+    n_batches = int(mnist.train.num_examples/batch_size)
+    for i in range(n_epochs): # train the model n_epochs times
+        total_loss = 0
 
-		for _ in range(n_batches):
-			X_batch, Y_batch = mnist.train.next_batch(batch_size)
-			# TO-DO: run optimizer + fetch loss_batch
-			# 
-			# 
-			total_loss += loss_batch
-		print 'Average loss epoch {0}: {1}'.format(i, total_loss/n_batches)
+        for _ in range(n_batches):
 
-	print 'Total time: {0} seconds'.format(time.time() - start_time)
+            X_batch, Y_batch = mnist.train.next_batch(batch_size)
+            _, loss_batch = sess.run([optimizer, loss], feed_dict={X: X_batch, Y: Y_batch})
+            total_loss += loss_batch
+            total_loss += loss_batch
+        print('Average loss epoch {0}: {1}'.format(i, total_loss/n_batches))
 
-	print('Optimization Finished!') # should be around 0.35 after 25 epochs
+    print ('Total time: {0} seconds'.format(time.time() - start_time))
+
+    print('Optimization Finished!') # should be around 0.35 after 25 epochs
 
 	# test the model
-	n_batches = int(mnist.test.num_examples/batch_size)
-	total_correct_preds = 0
-	for i in range(n_batches):
-		X_batch, Y_batch = mnist.test.next_batch(batch_size)
-		_, loss_batch, logits_batch = sess.run([optimizer, loss, logits], feed_dict={X: X_batch, Y:Y_batch}) 
-		preds = tf.nn.softmax(logits_batch)
-		correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y_batch, 1))
-		accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32)) # need numpy.count_nonzero(boolarr) :(
-		total_correct_preds += sess.run(accuracy)	
-	
-	print 'Accuracy {0}'.format(total_correct_preds/mnist.test.num_examples)
+    n_batches = int(mnist.test.num_examples/batch_size)
+    total_correct_preds = 0
+    for i in range(n_batches):
+        X_batch, Y_batch = mnist.test.next_batch(batch_size)
+        _, loss_batch, logits_batch = sess.run([optimizer, loss, logits], feed_dict={X: X_batch, Y:Y_batch})
+        preds = tf.nn.softmax(logits_batch)
+        correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y_batch, 1))
+        accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32)) # need numpy.count_nonzero(boolarr) :(
+        total_correct_preds += sess.run(accuracy)
+    print('Accuracy {0}'.format(total_correct_preds/mnist.test.num_examples))
